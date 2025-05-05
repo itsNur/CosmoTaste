56e6f51f0487ee8af9739eb9342d729476ba7dcb
Nursultan Adiev <adiev_n@mail.ru>
2025-05-05 22:35:46 +0600

    Add some docstrings

--- app/routes.py
+++ app/routes.py
@@ -5,11 +5,14 @@ from app.utils import load_recipes, save_recipes
 
 @app.route('/recipes', methods=['GET'])
 def get_recipes():
+    """Get all recipes"""
     recipes = load_recipes()
     return jsonify(recipes)
 
+
 @app.route('/recipes', methods=['POST'])
 def add_recipe():
+    """Add new recipe"""
     data = request.get_json()
     recipes = load_recipes()
 
@@ -27,16 +30,20 @@ def add_recipe():
     save_recipes(recipes)
     return jsonify(new_recipe), 201
 
+
 @app.route('/recipes/<int:recipe_id>', methods=['GET'])
 def get_recipe(recipe_id):
+    """Get recipe by id"""
     recipes = load_recipes()
     for recipe in recipes:
         if recipe['id'] == recipe_id:
             return jsonify(recipe)
     return jsonify({'error': 'Recipe not found'}), 404
 
+
 @app.route('/recipes/<int:recipe_id>', methods=['DELETE'])
 def delete_recipe(recipe_id):
+    """Delete recipe by id"""
     recipes = load_recipes()
     updated_recipes = [r for r in recipes if r['id'] != recipe_id]
 
@@ -46,8 +53,10 @@ def delete_recipe(recipe_id):
     save_recipes(updated_recipes)
     return jsonify({'message': 'Recipe deleted'}), 200
 
+
 @app.route('/recipes/<int:recipe_id>', methods=['PUT'])
 def update_recipe(recipe_id):
+    """Update recipe by id"""
     recipes = load_recipes()
     data = request.get_json()
 
